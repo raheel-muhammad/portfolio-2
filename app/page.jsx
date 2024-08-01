4d75a846b4990d56b9fc8b3ac91a05c00e00332f
raheel-muhammad <raheelmuhammadpervaiz@gmail.com>
2024-08-02 03:29:54 +0500

    changes done

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ const Home = () => {
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
-            <h1 className="h1 mb-6">Hi! I'm <br /> <span className="text-accent">Muhammad Raheel</span></h1>
+            <h1 className="h1 mb-6">Hi! I am <br /> <span className="text-accent">Muhammad Raheel</span></h1>
             <p className="max-w-[500px] mb-9 text-white/80">I excel at crafting elegant digital experiences and i am proficient in various programming languages and technologies.</p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <Button variant="outline" size="lg" className="uppercase flex items-center gap-2 hover:bg-accent hover:text-primary hover:transition-all duration-500">
